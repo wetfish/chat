c375e836bd7db486fe909b06fa4e599cebcc244a
Rachel Fish <itsrachelfish@gmail.com>
2013-04-21 20:25:31 +0400

    Stupid hack to scroll when images are posted

--- static/js/util.js
+++ static/js/util.js
@@ -1,18 +1,10 @@
-if(!String.linkify) {
-    String.prototype.linkify = function() {
-
-        // http://, https://, ftp://
-        var urlPattern = /\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gim;
-
-        // www. sans http:// or https://
-        var pseudoUrlPattern = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-
-        // Email addresses
-        var emailAddressPattern = /\w+@[a-zA-Z_]+?(?:\.[a-zA-Z]{2,6})+/gim;
+if(!String.fishformat) {
+    String.prototype.fishformat = function() {
+        var imgPattern = /\b(?:img|image)\s?\[(.+?)\]/gim;
+        var urlPattern = /\b(?:url|link)\s?\[(.+?)\]/gim;
 
         return this
-            .replace(urlPattern, '<a href="$&">$&</a>')
-            .replace(pseudoUrlPattern, '$1<a href="http://$2">$2</a>')
-            .replace(emailAddressPattern, '<a href="mailto:$&">$&</a>');
+            .replace(imgPattern, '<img src="$1">')
+            .replace(urlPattern, '<a href="$1">$1</a>');
     };
 }
